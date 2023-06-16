6faa5ae8b20a2c488d6bc5fc4257962aefb8f2de
halangbacca <halanbacca@outlook.com>
2023-06-16 14:42:44 -0300

    Ex 4 - Na Procedure ExibirTodosProdutos implementar exception

--- Exercícios M3-S07.sql
+++ Exercícios M3-S07.sql
@@ -1,6 +1,9 @@
 SELECT * FROM Produto;
 SELECT * FROM ProdutoPreco;
 
+DROP TABLE ProdutoPreco CASCADE CONSTRAINTS;
+
+
 -- Exercício 1
 DECLARE
 	v_id NUMBER;
@@ -17,35 +20,7 @@ BEGIN
     CLOSE cAtualizarStatusProduto;
 END;
 
--- Exercício 2
-DECLARE
-	v_id NUMBER;
-	v_status NUMBER;
-	v_valor NUMBER (10, 2);
-	CURSOR cListarDadosProdutoPreco IS SELECT Id, Status, Valor FROM ProdutoPreco;
-
-BEGIN    
-    OPEN cListarDadosProdutoPreco;
-        LOOP
-            FETCH cListarDadosProdutoPreco INTO v_id, v_status, v_valor;
-                EXIT WHEN cListarDadosProdutoPreco%NOTFOUND;
-                
-               	IF v_status = 1 THEN
-                	DBMS_OUTPUT.PUT_LINE('Status ativo');
-                ELSE
-                	DBMS_OUTPUT.PUT_LINE('Status inativo');
-                END IF;
-               
-               	IF v_valor < 1000.00 THEN
-                	DBMS_OUTPUT.PUT_LINE('Valor menor que R$ 1000,00');
-                ELSE
-                	DBMS_OUTPUT.PUT_LINE('Valor maior que R$ 1000,00');
-                END IF;
-        END LOOP;
-    CLOSE cListarDadosProdutoPreco;
-END;
-
--- Exercício 3
+-- Exercícios 2, 3 e 4
 CREATE OR REPLACE PROCEDURE ExibirTodosProdutos
 IS
 	v_id NUMBER;
@@ -70,7 +45,13 @@ BEGIN
                 	DBMS_OUTPUT.PUT_LINE('Valor maior que R$ 1000,00');
                 END IF;
         END LOOP;
-    CLOSE cListarDadosProdutoPreco;
+       
+     EXCEPTION
+     	WHEN NO_DATA_FOUND THEN
+     		DBMS_OUTPUT.PUT_LINE('Erro: dados não encontrados! ' || SQLCODE);
+       
+     CLOSE cListarDadosProdutoPreco;
+     	
 END;
 
 CALL SYSTEM.ExibirTodosProdutos();
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # M3-S07-PL-SQL-DEVinPhilips
+Exercícios da sexta semana do módulo 3 (full-stack) do DEVin[Philips].
