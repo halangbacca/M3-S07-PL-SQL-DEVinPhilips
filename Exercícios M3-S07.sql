55b531ba4ca983c68ff9ba58cb238bf2222018db
halangbacca <halanbacca@outlook.com>
2023-06-16 14:19:05 -0300

    Ex 3 - Criar Procedure com nome ExibirTodosProdutos

--- Exercícios M3-S07.sql
+++ Exercícios M3-S07.sql
@@ -44,3 +44,33 @@ BEGIN
         END LOOP;
     CLOSE cListarDadosProdutoPreco;
 END;
+
+-- Exercício 3
+CREATE OR REPLACE PROCEDURE ExibirTodosProdutos
+IS
+	v_id NUMBER;
+	v_status NUMBER;
+	v_valor NUMBER (10, 2);
+	CURSOR cListarDadosProdutoPreco IS SELECT Id, Status, Valor FROM ProdutoPreco;
+BEGIN    
+    OPEN cListarDadosProdutoPreco;
+        LOOP
+            FETCH cListarDadosProdutoPreco INTO v_id, v_status, v_valor;
+                EXIT WHEN cListarDadosProdutoPreco%NOTFOUND;
+                
+               	IF v_status = 1 THEN
+                	DBMS_OUTPUT.PUT_LINE('Status ativo');
+                ELSE
+                	DBMS_OUTPUT.PUT_LINE('Status inativo');
+                END IF;
+               
+               	IF v_valor < 1000.00 THEN
+                	DBMS_OUTPUT.PUT_LINE('Valor menor que R$ 1000,00');
+                ELSE
+                	DBMS_OUTPUT.PUT_LINE('Valor maior que R$ 1000,00');
+                END IF;
+        END LOOP;
+    CLOSE cListarDadosProdutoPreco;
+END;
+
+CALL SYSTEM.ExibirTodosProdutos();
