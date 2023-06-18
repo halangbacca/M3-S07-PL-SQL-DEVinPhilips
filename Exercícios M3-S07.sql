186964d5296e06ec37ab5186d3ff76edb49c024b
halangbacca <halanbacca@outlook.com>
2023-06-18 00:51:06 -0300

    Ex 5 - Crie uma Procedure chamada AtualizarProdutoPreco

--- Exercícios M3-S07.sql
+++ Exercícios M3-S07.sql
@@ -3,7 +3,6 @@ SELECT * FROM ProdutoPreco;
 
 DROP TABLE ProdutoPreco CASCADE CONSTRAINTS;
 
-
 -- Exercício 1
 DECLARE
 	v_id NUMBER;
@@ -21,37 +20,70 @@ BEGIN
 END;
 
 -- Exercícios 2, 3 e 4
-CREATE OR REPLACE PROCEDURE ExibirTodosProdutos
-IS
+CREATE OR REPLACE PROCEDURE ExibirTodosProdutos IS
 	v_id NUMBER;
 	v_status NUMBER;
 	v_valor NUMBER (10, 2);
-	CURSOR cListarDadosProdutoPreco IS SELECT Id, Status, Valor FROM ProdutoPreco;
-BEGIN    
-    OPEN cListarDadosProdutoPreco;
-        LOOP
-            FETCH cListarDadosProdutoPreco INTO v_id, v_status, v_valor;
-                EXIT WHEN cListarDadosProdutoPreco%NOTFOUND;
+	CURSOR cListarDadosProdutoPreco IS SELECT Id, Status, Valor FROM ProdutoPreco ORDER BY Valor;
+BEGIN
+	OPEN cListarDadosProdutoPreco;
+		LOOP
+			FETCH cListarDadosProdutoPreco INTO v_id, v_status, v_valor;
+				EXIT WHEN cListarDadosProdutoPreco%NOTFOUND;
                 
                	IF v_status = 1 THEN
-                	DBMS_OUTPUT.PUT_LINE('Status ativo');
+                	DBMS_OUTPUT.PUT_LINE('ID: ' || v_id || ' - status ativo');
                 ELSE
-                	DBMS_OUTPUT.PUT_LINE('Status inativo');
+                	DBMS_OUTPUT.PUT_LINE('ID: ' || v_id || ' - status inativo');
                 END IF;
                
                	IF v_valor < 1000.00 THEN
-                	DBMS_OUTPUT.PUT_LINE('Valor menor que R$ 1000,00');
+                	DBMS_OUTPUT.PUT_LINE('ID: ' || v_id || ' - valor menor que R$ 1000,00');
                 ELSE
-                	DBMS_OUTPUT.PUT_LINE('Valor maior que R$ 1000,00');
+                	DBMS_OUTPUT.PUT_LINE('ID: ' || v_id || ' - valor maior que R$ 1000,00');
                 END IF;
         END LOOP;
+    CLOSE cListarDadosProdutoPreco;
+
+    EXCEPTION
+    	WHEN NO_DATA_FOUND THEN
+    		DBMS_OUTPUT.PUT_LINE('Erro! Dados não encontrados');
+     		DBMS_OUTPUT.PUT_LINE('Código do erro: ' || SQLCODE);
        
-     EXCEPTION
-     	WHEN NO_DATA_FOUND THEN
-     		DBMS_OUTPUT.PUT_LINE('Erro: dados não encontrados! ' || SQLCODE);
-       
-     CLOSE cListarDadosProdutoPreco;
-     	
 END;
 
 CALL SYSTEM.ExibirTodosProdutos();
+
+-- Exercício 5
+CREATE OR REPLACE PROCEDURE AtualizarProdutoPreco(pIdProduto NUMBER, pValorProduto NUMBER) IS
+	ProdutoInexistente EXCEPTION;
+    StatusDesativado   EXCEPTION;
+   
+    PRAGMA EXCEPTION_INIT(ProdutoInexistente, -20001);
+    PRAGMA EXCEPTION_INIT(StatusDesativado, -20002);
+   
+BEGIN
+	FOR produto IN (SELECT * FROM ProdutoPreco) LOOP
+		IF produto.Id NOT IN (produto.Id) THEN
+			RAISE ProdutoInexistente;
+		ELSIF produto.Status = 0 THEN
+			RAISE StatusDesativado;
+		ELSE
+    		UPDATE ProdutoPreco SET Valor = pValorProduto WHERE Id = pIdProduto;
+    	END IF;
+    END LOOP;
+   
+EXCEPTION
+	WHEN ProdutoInexistente THEN
+		DBMS_OUTPUT.PUT_LINE('Produto inexistente!');
+		DBMS_OUTPUT.PUT_LINE('Código do erro: ' || SQLCODE);
+	WHEN StatusDesativado THEN
+		DBMS_OUTPUT.PUT_LINE('Produto está desativado na tabela!');
+		DBMS_OUTPUT.PUT_LINE('Código do erro: ' || SQLCODE);
+	
+END;
+
+CALL SYSTEM.AtualizarProdutoPreco(1, '100,00');
+CALL SYSTEM.AtualizarProdutoPreco(20, '20,00');
+
+SELECT * FROM ProdutoPreco;
